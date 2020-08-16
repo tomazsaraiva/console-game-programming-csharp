74978fe5e0d4ea14f31e1f1381cc15f2a1858af2
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2020-08-16 22:43:39 +0100

    add create player code

--- main.cs
+++ main.cs
@@ -1,9 +1,39 @@
-using System;
+﻿using System;
 
-class Program
-{
-  static void Main(string[] args)
-  {
-    Console.WriteLine("Hello World!");
+class MainClass {
+
+	enum PlayerType
+	{
+		Human,
+		Computer
+	}
+
+  public static void Main (string[] args) {
+
+		GamePrint.Box("Tic, Tac, Toe");
+
+		PlayerType player1Type = GetPlayerType();
+		string player1Name = GetPlayerName(player1Type);
+
+		PlayerType player2Type = GetPlayerType();
+		string player2Name = GetPlayerName(player2Type);
   }
+
+	private static PlayerType GetPlayerType()
+	{
+		GamePrint.Input("New Player\n1.Human\n2.Computer");
+		int playerType = GameInput.Number(new int[] { 1, 2 });
+		return (PlayerType)(playerType - 1);
+	}
+	private static string GetPlayerName(PlayerType playerType)
+	{
+		if(playerType == PlayerType.Computer)
+		{
+			return "Computer";
+		}
+		
+		GamePrint.Input("What's your name?");
+		string playerName = GameInput.Text();
+		return playerName;
+	}
 }
