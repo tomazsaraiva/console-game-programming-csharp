fa82c38630f25ce700084301858b438f24fa3947
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2020-09-17 23:13:33 +0100

    add hangman print

--- main.cs
+++ main.cs
@@ -4,6 +4,16 @@ class Program
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello World!");
+    Console.WriteLine("    .________.");
+    Console.WriteLine("    |/       |");
+    Console.WriteLine("    |        @");
+    Console.WriteLine("    |       /|\\");
+    Console.WriteLine("    |        |");
+    Console.WriteLine("    |       / \\");
+    Console.WriteLine("    |");
+    Console.WriteLine("____|____          WORD: _ A _ _ _ A _");
+
+    GamePrint.Input("Choose a letter:");
+    GameInput.Text();
   }
 }
